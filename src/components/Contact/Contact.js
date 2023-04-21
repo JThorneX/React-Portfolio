ce96f984d2ee694d67969303058af777db3dfa65
JThorneX <115962097+JThorneX@users.noreply.github.com>
2023-04-21 15:17:00 -0600

    contact form structure

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,6 +3,8 @@ import "./contact.css";
 import emailjs from "@emailjs/browser";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
+import { MdOutlineEmail } from "react-icons/md";
+import { BsLinkedin } from "react-icons/bs";
 
 function Contact() {
   const form = useRef();
@@ -19,24 +21,60 @@ function Contact() {
   };
 
   return (
-    <div>
+    <>
       <Navbar />
-      <div className="contact form">
-        <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name="name" placeholder="Full Name" required />
-          <input type="email" name="email" placeholder="Your Email" required />
-          <textarea
-            name="message"
-            rows="7"
-            placeholder="Your Message"
-            required
-          />
-          <button type="submit" className="btn btn-primary">
-            Send Message
-          </button>
-        </form>
+      <div className="all-contact">
+        <div className="contact-text">
+          <h1>Contact Me</h1>
+          <h3>
+            As a beginner in this industry, I am always looking for more
+            contacts, so feel free to drop me a line!
+          </h3>
+          {/* <div className="cyan-logo">
+          <img src={cyanlogo} alt="cyan logo" className="cyan-logo-img" />
+        </div> */}
+        </div>
+        <div className="contact-wrapper">
+          <div className="alt-options">
+            <article className="contact_option">
+              <MdOutlineEmail size="50" color="cyan" />
+              <h4>Email</h4>
+              <a href="mailto:jake.d.d.thorne@gmail.com" target="_blank">
+                Send an Email
+              </a>
+            </article>
+            <article className="contact_option">
+              <BsLinkedin size="50" color="cyan" />
+              <h4>Linkedin</h4>
+              <a href="https://linkedin.com" target="_blank">
+                Send a Message
+              </a>
+            </article>
+          </div>
+          <div className="contact-form">
+            <form ref={form} onSubmit={sendEmail}>
+              <input type="text" name="name" placeholder="Full Name" required />
+              <input
+                type="email"
+                name="email"
+                placeholder="Your Email"
+                required
+              />
+              <textarea
+                name="message"
+                rows="7"
+                placeholder="Your Message"
+                required
+              />
+              <button type="submit" className="btn btn-primary">
+                Send Message
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
