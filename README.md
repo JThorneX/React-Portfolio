c6e381314d5719eeacdc83220136e42544c052c9
JThorneX <115962097+JThorneX@users.noreply.github.com>
2023-04-21 10:33:14 -0600

    top half of experience done + updated readme

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 A portfolio showcasing my React and CSS skills as well as a selection of the work I've done over the past few months.
 
-
-
-
 ## Credits
 
 [For dark/light mode toggle](https://reactjsexample.com/day-and-night-light-dark-theme-switch-with-pretty-cool-animation-for-react/)
+
+[For the Experience Skill Bars](https://www.npmjs.com/package/react-skills)
+
+[For Particles](https://github.com/matteobruni/tsparticles)
--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,8 +1,17 @@
 import React from "react";
+import "./experience.css";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import fox from "../../assets/images/foxoutline.svg";
-// import { SkillBars } from "react-skills";
+import { SkillBars } from "react-skills";
+import { GrNode } from "react-icons/gr";
+import { SiJquery } from "react-icons/si";
+import { SiBootstrap } from "react-icons/si";
+import { BsGit } from "react-icons/bs";
+import { SiHandlebarsdotjs } from "react-icons/si";
+import { GrMysql } from "react-icons/gr";
+import { SiMongodb } from "react-icons/si";
+import { SiSequelize } from "react-icons/si";
 
 function Experience() {
   // const skills = [
@@ -21,30 +30,59 @@ function Experience() {
   //   },
   // };
 
-  // const skillsData = [
-  //   {
-  //     name: "ReactJS",
-  //     level: 100,
-  //     color: "cyan",
-  //     height: 10,
-  //     labelsWidth: 10,
-  //     width: 10,
-  //   },
-  //   {
-  //     name: "SomeOtherTech",
-  //     level: 85,
-  //     color: "red",
-  //   },
-  // ];
+  const skillsData = [
+    {
+      name: "HTML",
+      level: 85,
+      color: "cyan",
+      height: 5,
+    },
+    {
+      name: "CSS",
+      level: 70,
+      color: "cyan",
+    },
+    {
+      name: "React",
+      level: 70,
+      color: "cyan",
+    },
+    {
+      name: "Node.js",
+      level: 55,
+      color: "cyan",
+    },
+    {
+      name: "MySQL",
+      level: 45,
+      color: "cyan",
+    },
+  ];
   return (
     <div className="exp-wrapper">
       <Navbar />
-      <div className="experience">
+      <div className="exp-content">
         <h1>Experience & Skills</h1>
-        <div className="skill-bar">
-          {/* <SkillBars skills={skillsData} className="bar" /> */}
-          <div className="fox-img">
-            <img src={fox} alt="fox with shades" className="fox-image" />
+        <div className="fox-img">
+          <img src={fox} alt="fox with shades" className="fox-image" />
+        </div>
+        <div className="experience">
+          <div className="skill-bar">
+            <SkillBars skills={skillsData} className="bar" />
+          </div>
+          <div className="skill-cluster">
+            <div className-="firstrow-skill">
+              <GrNode className="icon" color="cyan" size="70" />
+              <SiJquery className="icon" color="cyan" size="70" />
+              <SiBootstrap className="icon" color="cyan" size="70" />
+              <BsGit className="icon" color="cyan" size="70" />
+            </div>
+            <div className="secondrow-skill">
+              <GrMysql className="icon" color="cyan" size="70" />
+              <SiMongodb className="icon" color="cyan" size="70" />
+              <SiSequelize className="icon" color="cyan" size="70" />
+              <SiHandlebarsdotjs className="icon" color="cyan" size="70" />
+            </div>
           </div>
         </div>
       </div>
