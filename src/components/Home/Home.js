86a96cc8c13e25bdfd6fec3a4b9c0119118b27e3
JThorneX <115962097+JThorneX@users.noreply.github.com>
2023-04-20 22:16:33 -0600

    particlesjs on home screen

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,87 @@
 import React from "react";
 import "./home.css";
 import Navbar from "../Navbar/Navbar";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import { Link } from "react-router-dom";
 
 function Home() {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
   return (
-    <div>
-      <Header />
-      <Navbar />
-
-      <div className="container home-container">
-        <div className="home-img"></div>
+    <>
+      <div className="position-fixed">
+        <Navbar />
+        <div className="container home-container">
+          <div className="home-content">
+            <h1>Jacob Thorne</h1>
+            <h3>Web Dev</h3>
+          </div>
+        </div>
       </div>
-
-      <Footer />
-    </div>
+      <div className="particles-wrapper">
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            background: {
+              color: {
+                value: "#0d47a1",
+              },
+            },
+            autoplay: true,
+            fpsLimit: 40,
+            particles: {
+              number: {
+                value: 80,
+                density: {
+                  enable: true,
+                  value_area: 800,
+                },
+              },
+              color: {
+                value: ["#fdcf58", "#757676", "#f27d0c", "#800909", "#f07f13"],
+              },
+              opacity: {
+                value: 0.5,
+                random: true,
+              },
+              size: {
+                value: 3,
+                random: true,
+              },
+              move: {
+                enable: true,
+                speed: 6,
+                random: false,
+              },
+            },
+            interactivity: {
+              events: {
+                onclick: {
+                  enable: true,
+                  mode: "push",
+                },
+                resize: true,
+              },
+            },
+            background: {
+              image: "radial-gradient(#4a0000, #000)",
+            },
+          }}
+        />
+      </div>
+    </>
   );
 }
 
