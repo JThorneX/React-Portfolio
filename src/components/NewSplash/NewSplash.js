492fc38f921f4121b85b64f71201d243bd0fbd30
JThorneX <115962097+JThorneX@users.noreply.github.com>
2023-10-03 12:37:48 -0600

    replaced anchor with navlink

--- src/components/NewSplash/NewSplash.js
+++ src/components/NewSplash/NewSplash.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./newsplash.css";
 import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import whitelogo from "../../assets/images/white logo jthorne.svg";
 
@@ -19,12 +20,12 @@ function NewSplash() {
         <img src={whitelogo} alt="white logo" className="white-logo-img" />
       </div>
       <div className="buttons">
-        <a href="React-Portfolio/summary">
+        <NavLink exact to="/summary">
           <button className="summary">Ain't Nobody Got Time For This</button>
-        </a>
-        <a href="React-Portfolio/home">
+        </NavLink>
+        <NavLink exact to="/home">
           <button className="fullSite">Show Me What You Got</button>
-        </a>
+        </NavLink>
       </div>
     </div>
   );
